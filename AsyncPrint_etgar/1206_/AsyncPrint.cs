b6efad135a7c3e2d359cf1781057a2c197c14d9c
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-06-12 21:34:34 +0300

    Add files via upload

---
+++ AsyncPrint_etgar/1206_/AsyncPrint.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1206_
+{
+    public class AsyncPrint
+    {
+        private Queue<string> allMessages;
+
+        private const int THREADS_COUNTER = 10;
+
+        public AsyncPrint()
+        {
+            allMessages = new Queue<string>();
+            for (int i = 0; i < THREADS_COUNTER; i++)
+            {
+                new Thread(() => { CheckPrintMessage(); }).Start();
+            }
+        }
+
+        public void AddMessage(string msg)
+        {
+            lock(this)
+            {
+                allMessages.Enqueue(msg);
+                Monitor.PulseAll(this);
+            }
+        }
+
+        public void CheckPrintMessage()
+        {
+            lock(this)
+            {
+                while (true)
+                {
+                    if (allMessages.Count > 0)
+                    {
+                        Console.WriteLine(allMessages.Dequeue());                        
+                    }
+                    Monitor.Wait(this);
+                }
+            }
+        }
+    }
+}
---
+++ AsyncPrint_etgar/1206_/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1206_
+{
+    class Program
+    {
+        interface I1
+        {
+            void foo();
+        }
+        class A : I1
+            {
+            public void foo()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class B : A, I1
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
