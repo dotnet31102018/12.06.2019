324a5d4caab873b9e041f3b1ce1cadc2fcc811d6
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-06-12 19:20:37 +0300

    Add files via upload

---
+++ AsyncPrint/1206_/AsyncPrint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1206_
+{
+    public class AsyncPrint
+    {
+        private Queue<string> allMessages;
+
+        public AsyncPrint()
+        {
+            allMessages = new Queue<string>();
+        }
+
+        public void AddMessage(string msg)
+        {
+            lock(this)
+            {
+                allMessages.Enqueue(msg);
+                Monitor.Pulse(this);
+            }
+        }
+
+        public void CheckPrintMessage()
+        {
+            lock(this)
+            {
+                while (allMessages.Count == 0)
+                {
+                    Monitor.Wait(this);
+                }
+                Console.WriteLine(allMessages.Dequeue());
+            }
+        }
+    }
+}
